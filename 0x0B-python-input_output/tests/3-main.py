273b728091484eac2a471357765882c58f82701e
vivJoe <vivianiwegbu@gmail.com>
2023-07-11 14:55:16 -0700

    python input and output

---
+++ 0x0B-python-input_output/tests/3-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+to_json_string = __import__('3-to_json_string').to_json_string
+
+my_list = [1, 2, 3]
+s_my_list = to_json_string(my_list)
+print(s_my_list)
+print(type(s_my_list))
+
